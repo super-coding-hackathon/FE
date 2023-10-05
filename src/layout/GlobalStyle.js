b2a0ce0d165d317a8492c6615e5c3269162feccf
Sim <0doogadooga@gmail.com>
2023-10-05 17:35:34 +0900

    초기세팅

---
+++ src/layout/GlobalStyle.js
@@ -0,0 +1,10 @@
+import { createGlobalStyle } from "styled-components";
+import reset from "styled-reset";
+
+const GlobalStyle = createGlobalStyle`
+:root{}
+
+${reset}
+`;
+
+export default GlobalStyle;
