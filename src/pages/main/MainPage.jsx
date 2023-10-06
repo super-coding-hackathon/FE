0b18ba05eadc8a366a96cf745a3a979db5e49b24
Sim <0doogadooga@gmail.com>
2023-10-06 10:31:31 +0900

    bootstrap 수정 및 env파일 추가

--- src/pages/main/MainPage.jsx
+++ src/pages/main/MainPage.jsx
@@ -1,7 +1,17 @@
 import React from "react";
+import Button from "react-bootstrap/Button";
+// import { Button } from "react-bootstrap";
 
 const MainPage = () => {
-  return <p>main</p>;
+  return (
+    <>
+      <Button variant="primary">Primary</Button>{" "}
+      <Button variant="success">Success</Button>{" "}
+      <Button as="input" type="button" value="Input" />{" "}
+      <Button variant="info">Info</Button>{" "}
+      <Button variant="outline-dark">Dark</Button>
+    </>
+  );
 };
 
 export default MainPage;
