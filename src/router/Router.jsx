ed5ff8a77247121d21112f52062dcff69b61b8c6
Sim <0doogadooga@gmail.com>
2023-10-07 16:31:36 +0900

    Router 수정

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -1,14 +1,23 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import GlobalStyle from "../layout/GlobalStyle";
-import MainPage from "../pages/main/MainPage";
+import Main from "../pages/main/MainPage";
 import RegisterPage from "../pages/register/RegisterPage";
+import GeneralLayout from "./../layout/GeneralLayout";
+import Apart from "./../pages/apart/Apart";
+import Studio from "./../pages/studio/Studio";
+import Office from "./../pages/office/Office";
 
 const Routeres = () => {
   return (
     <Router>
       <GlobalStyle />
       <Routes>
-        <Route path="/" element={<MainPage />} />
+        <Route path="/" element={<GeneralLayout />}>
+          <Route path="" element={<Main />} />
+          <Route path="1" element={<Apart />} />
+          <Route path="2" element={<Studio />} />
+          <Route path="3" element={<Office />} />
+        </Route>
         <Route path="/register" element={<RegisterPage />} />
       </Routes>
     </Router>
