c00435b155affd2d7d9301cdf93f0c97aa20d9b6
ckwlghks123 <ckwlghks12+github2@gmail.com>
2023-10-09 04:50:46 +0900

    fix: netlify proxy 설정파일 추가

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: `${process.env.REACT_APP_BASE_URL}/proxy/`,
+  baseURL: `${process.env.REACT_APP_BASE_URL}`,
 })
 
 api.interceptors.request.use((request) => {
